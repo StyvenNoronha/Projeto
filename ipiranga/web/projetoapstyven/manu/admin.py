fa7629277f85bd082eb788eb4a8429bf5e40c16e
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2022-11-17 10:05:47 -0300

    Delete ipiranga/web/projetoapstyven directory

--- ipiranga/web/projetoapstyven/manu/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Motor,Manutencao
-# Register your models here.
-admin.site.register(Motor)
-admin.site.register(Manutencao)
--- ipiranga/web/projetoapstyven/manu/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ManuConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'manu'
